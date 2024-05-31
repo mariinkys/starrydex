b3d62f3452745c853b835034b333d4108923b0b5
mariinkys <kysgeneral.nrgua@slmail.me>
2024-05-31 21:01:23 +0200

    utils code refactor

--- src/app.rs
+++ src/app.rs
@@ -3,6 +3,9 @@
 use std::collections::HashMap;
 
 use crate::fl;
+use crate::utils::{
+    capitalize_string, download_all_pokemon_sprites, load_all_pokemon, load_pokemon, scale_numbers,
+};
 use cosmic::app::{Command, Core};
 use cosmic::iced::alignment::{Horizontal, Vertical};
 use cosmic::iced::{Alignment, Length};
@@ -80,6 +83,12 @@ impl menu::action::MenuAction for MenuAction {
             MenuAction::Settings => Message::ToggleContextPage(ContextPage::Settings),
         }
     }
+}
+
+#[derive(Debug, Clone)]
+pub struct CustomPokemon {
+    pub pokemon: Pokemon,
+    pub sprite_path: Option<String>,
 }
 
 /// Implement the `Application` trait for your application.
@@ -461,136 +470,4 @@ impl CosmicDex {
         .padding([spacing.space_none, spacing.space_xxs])
         .into()
     }
-}
-
-#[derive(Debug, Clone)]
-pub struct CustomPokemon {
-    pokemon: Pokemon,
-    sprite_path: Option<String>,
-}
-
-async fn load_all_pokemon() -> Vec<CustomPokemon> {
-    let client = rustemon::client::RustemonClient::default();
-    let all_entries = rustemon::pokemon::pokemon::get_all_entries(&client)
-        .await
-        .unwrap_or_default();
-
-    let mut result = Vec::<CustomPokemon>::new();
-
-    for entry in all_entries {
-        let image_filename = format!("{}_front.png", entry.name);
-        let image_path = format!("resources/sprites/{}/{}", entry.name, image_filename);
-
-        result.push(CustomPokemon {
-            pokemon: Pokemon {
-                name: entry.name,
-                ..Default::default()
-            },
-            sprite_path: Some(image_path),
-        })
-    }
-
-    result
-}
-
-async fn load_pokemon(pokemon_name: String) -> CustomPokemon {
-    let client = rustemon::client::RustemonClient::default();
-    let pokemon = rustemon::pokemon::pokemon::get_by_name(pokemon_name.as_str(), &client)
-        .await
-        .unwrap_or_default();
-
-    let image_path = if let Some(front_default_sprite) = &pokemon.sprites.front_default {
-        // Only download the image if front_default sprite is available
-        Some(
-            download_image(front_default_sprite.to_string(), pokemon.name.to_string())
-                .await
-                .unwrap_or_else(|_| String::new()),
-        )
-    } else {
-        None
-    };
-
-    CustomPokemon {
-        pokemon: pokemon,
-        sprite_path: image_path, // Set default if image_path is None
-    }
-}
-
-async fn download_all_pokemon_sprites() {
-    let client = rustemon::client::RustemonClient::default();
-    let all_entries = rustemon::pokemon::pokemon::get_all_entries(&client)
-        .await
-        .unwrap_or_default();
-
-    for entry in all_entries {
-        let pokemon = rustemon::pokemon::pokemon::get_by_name(&entry.name, &client)
-            .await
-            .unwrap_or_default();
-
-        let _ = download_image(
-            pokemon.sprites.front_default.unwrap_or_default(),
-            pokemon.name.to_string(),
-        )
-        .await;
-    }
-}
-
-fn capitalize_string(input: &str) -> String {
-    let words: Vec<&str> = input.split('-').collect();
-
-    let capitalized_words: Vec<String> = words
-        .iter()
-        .map(|word| {
-            let mut chars = word.chars();
-            if let Some(first_char) = chars.next() {
-                first_char.to_uppercase().collect::<String>() + chars.as_str()
-            } else {
-                String::new()
-            }
-        })
-        .collect();
-
-    capitalized_words.join(" ")
-}
-
-fn scale_numbers(num: i64) -> f64 {
-    (num as f64) / 10.0
-}
-
-async fn download_image(
-    image_url: String,
-    pokemon_name: String,
-) -> Result<String, Box<dyn std::error::Error>> {
-    let image_filename = format!("{}_front.png", pokemon_name);
-    let image_path = format!("resources/sprites/{}/{}", pokemon_name, image_filename);
-
-    // file already downloaded?
-    if tokio::fs::metadata(&image_path).await.is_ok() {
-        return Ok(image_path);
-    }
-
-    let response = reqwest::get(&image_url).await?;
-
-    if response.status().is_success() {
-        let bytes = response.bytes().await?;
-
-        let path = std::path::PathBuf::from(&image_path);
-        if let Some(parent) = path.parent() {
-            tokio::fs::create_dir_all(parent).await?;
-        }
-
-        save_image(&image_path, &bytes).await?;
-        Ok(image_path)
-    } else {
-        Err(Box::new(std::io::Error::new(
-            std::io::ErrorKind::Other,
-            format!("Failed to download image. Status: {}", response.status()),
-        )))
-    }
-}
-
-async fn save_image(path: &str, bytes: &[u8]) -> std::io::Result<()> {
-    let mut file = tokio::fs::File::create(path).await?;
-    tokio::io::AsyncWriteExt::write_all(&mut file, bytes).await?;
-    Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ use app::CosmicDex;
 /// The `app` module is used by convention to indicate the main component of our application.
 mod app;
 mod core;
+mod utils;
 
 /// The `cosmic::app::run()` function is the starting point of your application.
 /// It takes two arguments:
