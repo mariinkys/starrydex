f467fd10b9ec01c546f91f0aab62d796a6e8c565
mariinkys <kysgeneral.nrgua@slmail.me>
2025-09-18 10:40:12 +0200

    chore: restore usual debug impl

--- src/entities/pokemon_info.rs
+++ src/entities/pokemon_info.rs
@@ -3,9 +3,15 @@ use std::fmt::Debug;
 use serde::{Deserialize, Serialize};
 
 /// Simple owned data structure, for displaying the Pokémon in the list page (main page)
-#[derive(Debug, Clone, Serialize, Deserialize)]
+#[derive(Clone, Serialize, Deserialize)]
 pub struct PokemonInfo {
     pub id: i64,
     pub name: String,
     pub sprite_path: Option<String>,
+}
+
+impl Debug for PokemonInfo {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.debug_struct("PokemonInfo").field("id", &self.id).finish()
+    }
 }
