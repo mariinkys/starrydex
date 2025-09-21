01e059fe59371704777925a45741a90c654d0ce3
mariinkys <kysgeneral.nrgua@slmail.me>
2025-09-21 11:38:16 +0200

    improv: remove models that we don't need for assetgen

--- assetgen/src/models/pokemon_info.rs
+++
@@ -1,8 +0,0 @@
-use serde::{Deserialize, Serialize};
-
-#[derive(Serialize, Deserialize)]
-pub struct PokemonInfo {
-    pub id: i64,
-    pub name: String,
-    pub sprite_path: Option<String>,
-}
--- assetgen/src/models/pokemon_type.rs
+++
@@ -1,7 +0,0 @@
-use serde::{Deserialize, Serialize};
-
-#[derive(Serialize, Deserialize)]
-pub struct PokemonType {
-    pub display_name: String,
-    pub name: String,
-}
